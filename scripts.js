8ac9163e5037c240641d8fd087f3a3a27113acd4
Nicole <nikidejourdanh@gmail.com>
2017-11-09 19:26:21 -0300

    segundo commit

--- scripts.js
+++ scripts.js
@@ -4,7 +4,9 @@ $(document).ready(function(){
 		event.preventDefault();
 		$(".list").prepend(
 			'<div class="tweet">'
-			+'<div class="cross"></div>'
+			+'<div class="cross">'
+			+'<a href="#" class="remove"><span class="glyphicon glyphicon-remove"></span></a>'
+			+'</div>'
 			+'<div class="heart">'
 			+'<a class="likeable" href="#"><span class="glyphicon glyphicon-heart"></span></a>'
 			+'<span id="counter">0</span>'
@@ -17,16 +19,16 @@ $(document).ready(function(){
 			
 	});
 
-	$('.list').on('click', '.cross', function(){
-		$(this).parent().fadeOut(500);
+	$('.list').on('click', '.remove', function(){
+		$(this).parent().parent().fadeOut(500);
 	})
 
 	$('.list').on('click', '.likeable', function(){
 		$(this).find('.glyphicon').css('color', '#F7819F')
 	})
 
-	$('.list').on('click', '.heart', function(){
-		$(this).find('#counter').html(function(i, val){
+	$('.list').on('click', '.likeable', function(){
+		$(this).parent().find('#counter').html(function(i, val){
 			return val * 1 + 1
 		})
 	})
